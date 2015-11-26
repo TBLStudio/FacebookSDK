13e9ff1860e4c1900fb13f2e84068a818123dd59
TBLStudio <iosdev.tbl@gmail.com>
2015-11-26 13:46:11 +0700

    add some example

--- HelloFBSDK/HelloFBSDK/ViewController.swift
+++ HelloFBSDK/HelloFBSDK/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    @IBOutlet weak var loginButton: FBSDKLoginButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        loginButton.readPermissions = ["public_profile", "email", "user_friends", "read_page_mailboxes", "user_posts"]
+        loginButton.delegate = self
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            returnUserData()
+            // User is already logged in, do work such as go to next view controller.
+        }
+        else
+        {
+            
+        }
+        
+        /*
         let loginButton: FBSDKLoginButton = FBSDKLoginButton()
         loginButton.center = self.view.center
         self.view.addSubview(loginButton)
@@ -39,6 +56,8 @@ class ViewController: UIViewController {
         sendButton.shareContent = content
         self.view.addSubview(sendButton)
         
+        */
+        
         
        
         
@@ -54,7 +73,10 @@ class ViewController: UIViewController {
 
     @IBAction func shareAction(sender: AnyObject) {
         //Share Dialog
-
+        shareFacebook()
+    }
+    
+    func shareFacebook() {
         let content: FBSDKShareLinkContent = FBSDKShareLinkContent()
         content.contentURL = NSURL(string: "http://mp3.zing.vn/bai-hat/Love-To-Be-Loved-By-You-Marc-Terenzi/ZWZ9CC86.html")!
         content.contentTitle = "Test facebook sdk"
@@ -65,7 +87,83 @@ class ViewController: UIViewController {
         dialog.fromViewController = self
         dialog.mode = FBSDKShareDialogMode.ShareSheet
         dialog.show()
+        
     }
+    
+    @IBAction func loginFacebook(sender: AnyObject) {
+        
+    }
+    
+    
+    //Facebook Delegate
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+                //returnUserData()
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+    }
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email,name"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                print("User Email is: \(userEmail)")
+                let userID: String = result.valueForKey("id") as! String
+                self.getFriendRequest(userID)
+                
+            }
+        })
+    }
+    
+    func getFriendRequest(userID: String) {
+        let fbRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "\(userID)/inbox", parameters: nil)
+        fbRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched friend request: \(result)")
+                
+                
+            }
+        })
+    }
+    
+    
 
 }
 
